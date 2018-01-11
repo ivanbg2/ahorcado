32bc1fdf2335f98bef9a049e28ac65b02d8c7569
Ivan Buendia <ivanbuendiaguillen@gmail.com>
2018-01-11 10:01:38 +0100

    ahorcado

---
+++ juego.php
@@ -0,0 +1,37 @@
+<?php
+
+//var_dump($_POST);
+$incognita = htmlspecialchars($_POST['incognita']);
+$letra = isset($_POST['letra']) ? htmlspecialchars($_POST['letra']) : '';
+$letras = isset($_POST['letras']) ? $_POST['letras'] : array();
+$letras[]=$letra;
+
+?>
+<h1>
+<?php
+for($i=0; $i<strlen($incognita); $i++){
+ 
+    $coincidencia=false;
+    foreach($letras as $letra){
+        if($incognita[$i] ===$letra){
+            echo $letra . " ";
+            $coincidencia=true;
+        }
+    }
+    if(!$coincidencia){
+        echo"_ ";
+    }
+}
+?>
+</h1>
+
+<form action="" method="post">
+    <input type="hidden" name="incognita" value="<?php echo $incognita ?>">
+        <?php for($j=0; $j < count($letras); $j++) :?>
+    <input type="hidden" name="letras[<?php echo $j?>]" value="<?php echo $letras[$j]?>" >
+        <br> <?php echo $letras[$j]?> 
+        <?php endfor;?>
+    <br><label for="letra">Escriba una letra</label>
+    <input type="text" name="letra" maxlength="1" size="1">
+    <input type="submit" value="Enviar">
+</form>
