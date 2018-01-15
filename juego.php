15da6f4aaf1020fe60d4d900a8143bf20183d3d9
Ivan Buendia <ivanbuendiaguillen@gmail.com>
2018-01-15 07:29:30 +0100

    capturaErrores

--- juego.php
+++ juego.php
@@ -2,13 +2,17 @@
 
 //var_dump($_POST);
 $incognita = htmlspecialchars($_POST['incognita']);
+$vidas = isset($_POST['vidas']) ? (int)($_POST['vidas']) : 7;
 $letra = isset($_POST['letra']) ? htmlspecialchars($_POST['letra']) : '';
 $letras = isset($_POST['letras']) ? $_POST['letras'] : array();
-$letras[]=$letra;
+if(preg_match("%[a-z]%", $letra) && !in_array($letra, $letras))
+  $letras[]=$letra;
 
 ?>
 <h1>
 <?php
+$aciertos = 0;
+$enEstaOcasionHaAcertado = false;
 for($i=0; $i<strlen($incognita); $i++){
  
     $coincidencia=false;
@@ -16,17 +20,37 @@ for($i=0; $i<strlen($incognita); $i++){
         if($incognita[$i] ===$letra){
             echo $letra . " ";
             $coincidencia=true;
+            $aciertos++;
+            $enEstaOcasionHaAcertado = true;
         }
     }
     if(!$coincidencia){
         echo"_ ";
     }
 }
+if(!$enEstaOcasionHaAcertado){
+$vidas--;
+}
 ?>
 </h1>
 
+<?php if($aciertos === strlen($incognita)) : ?>
+
+<h2>
+    Ganaste!!!!
+    <a href="index.html">Otra palabra</a>
+</h2>
+
+<?php elseif($vidas === 0): ?>
+<h2>
+    Perdiste!!!!
+    <a href="index.html">Otra palabra</a>
+</h2>
+
+<?php else : ?>
 <form action="" method="post">
     <input type="hidden" name="incognita" value="<?php echo $incognita ?>">
+    <input type="hidden" name="vidas" value="<?php echo $vidas ?>">
         <?php for($j=0; $j < count($letras); $j++) :?>
     <input type="hidden" name="letras[<?php echo $j?>]" value="<?php echo $letras[$j]?>" >
         <br> <?php echo $letras[$j]?> 
@@ -40,3 +64,4 @@ for($i=0; $i<strlen($incognita); $i++){
     </select>
     <input type="submit" value="Enviar">
 </form>
+<?php endif; ?>
